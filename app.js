9525f76ae2467f3030ccc3a8eed92ab2368fa72d
Mehmet Ugur Satir <musatir@gmail.com>
2023-09-03 23:24:48 +0200

    cors is removed since we're using api with own...

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ require('express-async-errors');
 
 // extra security packages
 const helmet = require('helmet');
-const cors = require('cors');
 const xss = require('xss-clean');
 
 
@@ -22,7 +21,6 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
 app.use(helmet());
-app.use(cors());
 app.use(xss());
 
 // routes
