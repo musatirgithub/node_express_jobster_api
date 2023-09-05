8966c70d221282c3c3dd31553d0a49a985eff67e
Mehmet Ugur Satir <musatir@gmail.com>
2023-09-05 18:47:35 +0200

    login controller's resp user obj updated

--- controllers/auth.js
+++ controllers/auth.js
@@ -24,7 +24,7 @@ const login = async (req, res) => {
   }
   // compare password
   const token = user.createJWT()
-  res.status(StatusCodes.OK).json({ user: { name: user.name }, token })
+  res.status(StatusCodes.OK).json({ user: { name: user.name, email:user.email, lastName:user.lastName, location:user.location, token } })
 }
 
 module.exports = {
