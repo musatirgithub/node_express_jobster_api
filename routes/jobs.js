f14bfeb6814e79596b8db41d4ead25eb9b30e417
Mehmet Ugur Satir <musatir@gmail.com>
2023-09-08 23:36:00 +0200

    job create, update, del func restricted for demo

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const testUser = require('../middleware/testUser')
 
 const router = express.Router()
 const {
@@ -9,8 +10,8 @@ const {
   getJob,
 } = require('../controllers/jobs')
 
-router.route('/').post(createJob).get(getAllJobs)
+router.post('/', testUser,createJob).get('/', getAllJobs)
 
-router.route('/:id').get(getJob).delete(deleteJob).patch(updateJob)
+router.get('/:id',getJob).delete('/:id',testUser, deleteJob).patch('/:id',testUser, updateJob)
 
 module.exports = router
