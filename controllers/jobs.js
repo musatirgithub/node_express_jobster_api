288e50f7f88a3ad5546a1c3a6285e1f1caa4d06f
Mehmet Ugur Satir <musatir@gmail.com>
2023-09-10 19:58:00 +0200

    completed

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -138,7 +138,12 @@ const showStats = async (req,res)=>{
     {$limit: 6}
   ])
 
+  monthlyApplications = monthlyApplications.map((item)=>{
+    const {_id : {year, month}, count} = item;
 
+    const date = moment().month(month -1).year(year).format('MMM Y')
+    return {date, count}
+  }).reverse()
 
   console.log(monthlyApplications)
 
